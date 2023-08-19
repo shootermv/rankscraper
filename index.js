b0d47b348d0658535266f471d2653bd916738d46
Moshe <shootermv@gmail.com>
2023-08-19 20:32:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+for run successfully this scraper needs 2 things to exist on the page:
+1. to find the apgination buttons
+2. to find the Rank Types drop down
+*/
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const converter = require("json-2-csv");
@@ -5,31 +10,30 @@ const domain = "RS0506";
 //const URL_TO_SCRAPE = `https://www.shanghairanking.com/rankings/gras/2022/${domain}`;
 //const URL_TO_SCRAPE = `https://www.shanghairanking.com/rankings/arwu/2022`
 
-const BASE = `https://www.shanghairanking.com/rankings/gras`;
+const BASE = `https://www.shanghairanking.com/rankings/arwu`;
 const DOMAIN = `RS0103`;
 
-//const URL_TO_SCRAPE = 'https://www.shanghairanking.com/rankings/arwu/2020'
-//const FILE_TO_SAVE = `./data/ranks_arwu_2020.csv`
 
-const savePages = async (year, domain) => {
+
+const scrapePages = async (year, domain) => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const URL_TO_SCRAPE = `${BASE}/${year}/${domain}`;
-  //console.log('---' + URL_TO_SCRAPE)
-  await page.goto(URL_TO_SCRAPE);
-  // gets last page
-  let pagesCountBtn = await page.waitForSelector(
+  const pup_page = await browser.newPage();
+  const URL_TO_SCRAPE = `${BASE}/${year}${domain? `/${domain}`: ''}`;
+  console.log('-STARTED SCRAPING- ' + URL_TO_SCRAPE)
+  await pup_page.goto(URL_TO_SCRAPE);
+  // gets total num of pages
+  let pagesCountBtn = await pup_page.waitForSelector(
     `.ant-pagination li:nth-child(8) a`
   );
   const pagesCount = await pagesCountBtn.evaluate((el) => el.textContent);
 
   // loop through all the pages
-
   let json = [];
-  let typesCount = (await page.$$(`thead th:nth-child(5) ul li`)).length;
-  for (let i = 1; i <= pagesCount; i++) {
-    let list = await readPage(i, page, typesCount);
-    console.log(`page ${i} saved`);
+  let typesCount = (await pup_page.$$(`thead th:last-child ul li`)).length;
+
+  for (let page = 1; page <= pagesCount; page++) {
+    let list = await readPage(page, pup_page, typesCount);
+    console.log(`trying to scrape page ${page}`);
     json = [...json, ...list];
   }
 
@@ -45,11 +49,13 @@ const savePages = async (year, domain) => {
 
   browser.close();
 };
-const readPage = async (pg, page, typesCount) => {
-  let json; // 5 types from drop down
+
+//reading each page of pagination & all its score types
+const readPage = async (pageNum, pup_page, typesCount) => {
+  let json; // all types from drop down
 
   for (let typeNum = 1; typeNum <= typesCount; typeNum++) {
-    let list = await readPageRankTypes(typeNum, pg, page);
+    let list = await readPageRankForTypes(typeNum, pageNum, pup_page);
     if (!json?.length) {
       // first time
       json = list;
@@ -63,57 +69,61 @@ const readPage = async (pg, page, typesCount) => {
 
   return json;
 };
-const readPageRankTypes = async (rnkNum, pgNum, page) => {
+
+const readPageRankForTypes = async (rnkNum, pgNum, page) => {
   // save every score of type at this page
 
-  // click on pagination button
-  const paginationBtn = await page.waitForSelector(
-    `.ant-pagination li[title="${pgNum}"] a`
-  );
-  await paginationBtn.click();
-
-  // click at dropDown arrow to open drop down
-  const img = await page.waitForSelector(
-    "thead th:nth-child(5) .rank-select img"
-  );
-  await img.click();
-
-  // select specific option from drop down popup
-  const option = await page.waitForSelector(
-    `thead th:nth-child(5) li:nth-child(${rnkNum})`
-  );
-  await option.click();
-
-  //const element = await page.waitForSelector('thead th:nth-child(6) .rank-select input');
-  //const rnk = await element.evaluate(el => el.value);
-
-  const list = await page.evaluate(() => {
-    // value of slected item at DropDown
-    let scorType = document
-      .querySelectorAll(".rank-select")[2]
-      .querySelector("input").value;
-
-    return [...document.querySelectorAll(".rk-table tbody tr")].map(
-      (row, idx) => {
-        //let rank = a.querySelector('.ranking').textContent.trim();
-        let univ = "---";
-        let scor = "---";
-        try {
-          univ = row.querySelector(".tooltiptext").textContent.trim();
-          // column of score - 5
-          scor = row.querySelector("td:nth-child(5)").textContent.trim();
-        } catch (err) {
-          console.log(err, idx);
-        }
-        return { /*rank,*/ univ, [scorType]: scor };
-      }
+  try {
+    // click on pagination button
+    const paginationBtn = await page.waitForSelector(
+      `.ant-pagination li[title="${pgNum}"] a`
     );
-  });
-  // console.log(JSON.stringify(list, null, 5))
-  return list;
+    await paginationBtn.click();
+
+    // click at dropDown arrow to open drop down
+    const img = await page.waitForSelector(
+      `thead th:last-child .rank-select img`
+    );
+    await img.click();
+
+    // select specific item from drop down popup
+    const option = await page.waitForSelector(
+      `thead th:last-child li:nth-child(${rnkNum})`
+    );
+    await option.click();
+
+    const list = await page.evaluate(() => {
+      // value of selected item at DropDown
+      let scorType = document.querySelector('thead th:last-child .rank-select input').value;
+  
+
+      return [...document.querySelectorAll(".rk-table tbody tr")].map(
+        (row, idx) => {
+          let univ = "---";
+          let scor = "---";
+          try {
+            univ = row.querySelector(".tooltiptext").textContent.trim();
+            // column of score - 5
+            scor = row.querySelector("td:last-child").textContent.trim();
+          } catch (err) {
+            console.log(err, idx);
+          }
+          return { /*rank,*/ univ, [scorType]: scor };
+        }
+      );
+    });
+
+    return list
+  } catch(err) {
+    console.log('ERROR: ', err);
+    return []
+  }
+  
+  //return list;
 };
+
 (async function () {
-  for (let yr = 2021; yr <= 2022; yr++) {
-    await savePages(`${yr}`, "RS0210");
+  for (let yr = 2023; yr <= 2023; yr++) {
+    await scrapePages(`${yr}`, "");
   }
 })();
